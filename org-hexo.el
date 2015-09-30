a5d26b7cb17de0b2303e99709bfa29ebc36541ab
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-09-30 14:13:27 +0800

    Move more methods from emacs-blogit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- org-hexo.el
+++ org-hexo.el
@@ -44,35 +44,50 @@
 
 ;;;; Custom
 
-(defcustom org-hexo-date-format "%Y-%02m-%02d %02H:%02M:%02S"
-  "Format use in #+DATE: metadata."
-  :group 'org-hexo
-  :type 'string)
+(defcustom org-hexo-project-alist nil
+  "Project list for org-hexo, it can contains many project, all
+  project must config like following:
 
-;; FIXME: merge with `org-hexo-enable-htmlize'
-(defcustom org-hexo-htmlize-src-block nil
-  "Enable to use htmlize to render src block.
-This one is global variable, if you want to make some page use htmlize, you
-can specify `#+HTMLIZE: true' in your file"
+(setq org-hexo-project-alist
+      '((\"coldnew's blog\" :config \"~/Workspace/blog/config.el\")))
+"
   :group 'org-hexo
-  :type 'boolean)
+  :type 'list)
 
-(defcustom org-hexo-overwrite-updated nil
+
+;;;; Config Variables
+;; NOTE: These variable should be defined in user's config.el file
+
+(defvar org-hexo-output-directory nil
+  "This variable should be defined in user's blogit config.el.
+This path also contains org-mode's `~/.org-timestamp' file as cache.")
+
+
+(defvar org-hexo-clear-ouput-when-republish nil
+  "When t, clean files in `org-hexo-ouput-directory' when republish
+project.")
+
+
+(defvar org-hexo-overwrite-updated nil
   "Enable this to make org-hexo auto add `updated:' in markdown
 file when not specify,the value will fetch from `date:'
-front-matter."
-  :group 'org-hexo
-  :type 'boolean)
+front-matter.")
 
-(defcustom org-hexo-permalink-format ""
-  "Format use in #+PERMALINK: metadata."
-  :group 'org-hexo
-  :type 'string)
+(defvar org-hexo-date-format "%Y-%02m-%02d %02H:%02M:%02S"
+  "Format use in #+DATE: metadata.")
 
-(defcustom org-hexo-newpost-template nil
-  "The template file for create hexo newpost."
-  :group 'org-hexo
-  :type 'string)
+(defvar org-hexo-permalink-format ""
+  "Format use in #+PERMALINK: metadata.")
+
+(defvar org-hexo-newpost-template nil
+  "The template file for create hexo newpost.")
+
+
+;;;; Internal Variables
+
+(defvar org-hexo-cache-filelist nil
+  "List to storage where to remove the cache file. This variable
+  will be rebuilf in `org-hexo--select-project'")
 
 
 ;;; Internal helper functions
@@ -139,6 +154,84 @@ If parsing failed, use regexp to get the options, else return nil.
       (when (re-search-forward match-regexp nil t)
         (match-string-no-properties 2 nil)))))
 
+(defun org-hexo--clear-private-variables ()
+  "Clear all private variables in blogit."
+  (setq
+   ;; Config variables
+   org-hexo-output-directory nil
+   org-hexo-clear-ouput-when-republish nil
+   org-hexo-overwrite-updated nil
+   org-hexo-date-format "%Y-%02m-%02d %02H:%02M:%02S"
+   org-hexo-permalink-format ""
+   org-hexo-newpost-template nil
+   ;; Internal Variables
+   org-hexo-cache-filelist nil))
+
+(defun org-hexo--select-project (func &optional msg)
+  "Prompt to select project to publish. If only one project
+list in `org-hexo-project-alist', do not prompt."
+  (let ((project
+         (if (= (length org-hexo-project-alist) 1)
+             (list (car org-hexo-project-alist))
+           (list
+            (assoc (org-icompleting-read
+                    (or msg "Publish blogit project: ")
+                    org-hexo-project-alist nil t)
+                   org-hexo-project-alist)
+            current-prefix-arg))))
+    ;; clear all private variable before load config file.
+    (org-hexo--clear-private-variables)
+    ;; load config according blogit-project-list
+    (let ((config (plist-get (cdar project) :config)))
+      (if config (load config)
+        (error (format "config %s not exist") config)))
+
+    (let ((project-list (list org-hexo-publish-project-alist)))
+      ;; rebuild cache filelist
+      (setq org-hexo-cache-filelist nil)
+      (dolist (c (car project-list))
+        (add-to-list
+         'org-hexo-cache-filelist
+         (f-join org-hexo-output-directory (concat (car c) ".cache"))))
+
+      (mapc
+       (lambda (current-project)
+         (funcall func current-project))
+
+       (org-publish-expand-projects project-list)))))
+
+(defun org-hexo--publish-project (project &optional force)
+  "Publush all blogit post, if post already posted and not modified,
+skip it.
+
+When force is t, re-publish selected blogit project."
+  (let ((org-publish-project-alist project)
+        (org-publish-timestamp-directory
+         (file-name-as-directory org-hexo-output-directory)))
+
+    ;; when repiblish blogit project, we need to remove all already exist cache
+    ;; file store in `org-hexo-cache-filelist'
+    (when force
+      ;; clear cache file
+      (dolist (c org-hexo-cache-filelist)
+        (if (file-exists-p c) (delete-file c)))
+      ;; if option on, clean all files in `org-hexo-output-directory'.
+      (when org-hexo-clear-ouput-when-republish
+        (let ((target-dir
+               (cond
+                ;; if target is symlink, remove symlink dir and recreate it
+                ((f-symlink? org-hexo-output-directory) (file-symlink-p org-hexo-output-directory))
+                ;; delete directory and recreate it
+                ((f-directory? org-hexo-output-directory) org-hexo-output-directory)
+                ;; FIXME: recreate it ?
+                (t (error "BUG: unknown remove org-hexo-output-directory methd.")))))
+          ;; delete target-dir and recreate it
+          (f-delete target-dir t)
+          (f-mkdir target-dir)))
+
+      ;; publish all current project
+      (org-publish-all force))))
+
 
 ;;;; Load all hexo exporter functions
 ;;
@@ -166,14 +259,14 @@ If parsing failed, use regexp to get the options, else return nil.
   "Update #+DATE: tag with current date info."
   (interactive)
   (org-hexo--set-option :date
-                     (format-time-string org-hexo-date-format)))
+                        (format-time-string org-hexo-date-format)))
 
 ;;;###autoload
 (defun org-hexo-update-modified ()
   "Update #+UPDATED: tag with current date info."
   (interactive)
   (org-hexo--set-option :updated
-                     (format-time-string org-hexo-date-format)))
+                        (format-time-string org-hexo-date-format)))
 
 ;; FIXME:
 ;;;###autoload
@@ -193,7 +286,16 @@ If parsing failed, use regexp to get the options, else return nil.
 (defun org-hexo-publish (&optional force)
   "Published modified blogit files."
   (interactive)
-  )
+  (org-hexo--select-project 'org-hexo--publish-project))
+
+;;;###autoload
+(defun oeg-hexo-republish (&optional force)
+  "Re-publish all blogit files."
+  (interactive)
+  (noflet ((org-hexo--republish-project
+            (project-list)
+            (org-hexo--publish-project project-list t)))
+    (org-hexo--select-project 'org-hexo--republish-project)))
 
 ;;;###autoload
 (defun org-hexo-publish-current-post (&optional force)
@@ -201,14 +303,6 @@ If parsing failed, use regexp to get the options, else return nil.
   (interactive)
   ;; Check if current post is belong to org-hexo.
   )
-
-;;;###autoload
-(defun oeg-hexo-republish (&optional force)
-  "Re-publish all blogit files."
-  (interactive)
-  )
-
-
 
 ;;;; TODO: remove blogit depends
 ;; ref: https://github.com/RNAer/org-blog/blob/master/ox-blog.el
